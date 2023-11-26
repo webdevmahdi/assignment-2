e875249b91edf2ca45b3b70e114c073aeaca29d3
webdevmahdi <mahdihasan01705259@gmail.com>
2023-11-26 12:17:52 +0600

    User interface and user Model has complete

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type UserData = {
+    userId: number,
+    username: string,
+    password: string,
+    fullName: {
+        firstName: string,
+        lastName: string
+    },
+    age: number,
+    email: string,
+    isActive: boolean,
+    hobbies: [
+        string,
+        string
+    ],
+    address: {
+        street: string,
+        city: string,
+        country: string,
+    },
+    orders: {
+        productName: string,
+        price: number,
+        quantity: number,
+    }
+}
+
+export default UserData;
---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose";
+import UserData from "./user.interface";
+
+const userSchema = new Schema({
+    userId: { type: Number, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    fullName: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+    },
+    age: { type: Number, required: true },
+    email: { type: String, required: true },
+    isActive: { type: Boolean, required: true },
+    hobbies: [{ type: String, required: true }],
+    address: {
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    orders: [
+        {
+            productName: { type: String, required: true },
+            price: { type: Number, required: true },
+            quantity: { type: Number, required: true },
+        },
+    ],
+});
